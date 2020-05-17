f094f37617b7ef76128366d273f97e114a667074
sauravk7077 <sauravk7077@gmail.com>
2020-05-17 08:54:35 +0530

    added dotenv, made registration form, login form

--- flask_site.py
+++ flask_site.py
@@ -1,5 +1,13 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+#To import the secret key
+from dotenv import load_dotenv
+import os
+load_dotenv()
+app.config['SECRET_KEY'] = os.getenv('KEY')
+
 
 posts = [
     {
@@ -18,14 +26,26 @@ posts = [
 
 @app.route('/')
 @app.route('/home')
-def hello_world():
-    return render_template('home.html',posts=posts)
+def home_page():
+    return render_template('home.html', posts=posts)
 
 
 @app.route('/about')
 def about_page():
     return render_template('about.html', title='About')
 
+@app.route('/register', methods=['GET','POST'])
+def register_page():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f"Account created for {form.username.data}", 'success')
+        return redirect(url_for('home_page'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/login')
+def login_page():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
