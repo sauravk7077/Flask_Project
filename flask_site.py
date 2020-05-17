b31d2c08be34ac6bddc5d779b0212a9d1acc53ca
sauravk7077 <sauravk7077@gmail.com>
2020-05-17 09:25:01 +0530

    changed static reference to url_for reference

--- flask_site.py
+++ flask_site.py
@@ -42,9 +42,15 @@ def register_page():
         return redirect(url_for('home_page'))
     return render_template('register.html', title='Register', form=form)
 
-@app.route('/login')
+@app.route('/login', methods=['GET','POST'])
 def login_page():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.username.data == 'addkj' and form.password.data == 'password':
+            flash('You have been logged in', 'success')
+            return redirect(url_for('home_page'))
+        else:
+            flash('Login unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', title='Login', form=form)
 
 if __name__ == "__main__":
