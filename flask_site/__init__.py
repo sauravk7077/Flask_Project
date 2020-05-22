6eff087cd6d644ef98f6e1245b3b99a7b4c3c4da
sauravk7077 <sauravk7077@gmail.com>
2020-05-22 08:27:27 +0530

    pagination, update details, create, update post et

--- flask_site/__init__.py
+++ flask_site/__init__.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, url_for, flash, redirect
-
+from flask import Flask
 from dotenv import load_dotenv
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 import os
 
 
@@ -13,3 +14,9 @@ load_dotenv()
 app.config['SECRET_KEY'] = os.getenv('KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "login_page"
+login_manager.login_message_category= 'info'
+
+from flask_site import routes
--- run.py
+++ run.py
@@ -2,6 +2,3 @@ from flask_site import app
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-from flask_site import routes
