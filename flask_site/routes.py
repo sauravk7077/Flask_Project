a614b7f5b03741380b803f58ce423156360bbf8b
sauravk7077 <sauravk7077@gmail.com>
2020-05-22 11:18:59 +0530

    added user_post

--- flask_site/routes.py
+++ flask_site/routes.py
@@ -140,11 +140,11 @@ def delete_post(post_id):
     return redirect(url_for('home_page'))
 
 
-@app.route('/user/<str:username>')
+@app.route('/user/<string:username>', methods=['GET','POST'])
 def user_posts(username):
     page = request.args.get('page',default=1, type=int)
     user = User.query.filter_by(username=username).first_or_404()
     posts = Post.query.filter_by(author=user)\
         .order_by(Post.date_posted.desc())\
         .paginate(page=page, per_page=10)
-    return render_template('home.html', posts=posts)
+    return render_template('user_post.html', posts=posts, user=user)
